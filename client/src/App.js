fd3bb7cad6f3849656eb348797153b5060125666
Manny <icuxxx2004@yahoo.com>
2019-05-20 10:29:46 -0700

    add changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Navbar from "./Components/Navbar";
 import Jumbotron from './Components/Jumbotron';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from './Components/Button';
+
 
 function App() {
   return (
     <div className="App">
     <Navbar/>
     <Jumbotron/>
+    <Button/>
     </div>
   );
 }
