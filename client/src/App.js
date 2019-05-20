fceb9e944a33d744a14b830fe35c71a1df7ec775
Samara Gideon <sgideon1@yahoo.com>
2019-05-20 08:50:21 -0700

    random adjustments

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Nav from './components//Nav';
+
+import Jumbotron from './components/Jumbotron';
+
 
 function App() {
+
   return (
+
     <div className="App">
-      Hello MoodGroove
+
+      <Nav/>
+
+      <Jumbotron/>
+
+      <button className = 'btn btn-primary'>Submit</button>
+
     </div>
+
   );
+
 }
 
 export default App;
--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -6,7 +6,31 @@ export default function Nav() {
 
     <div>
 
-        Nav
+        <nav className ='navbar navbar-expand-sm' style = {{background: 'rgba(255, 178, 78, 0.883)'}}>
+
+            <ul className ="navbar-nav justify-content-center">
+
+                <li className ="nav-item">
+
+                    <a className ="nav-link" href="#"><button style = {{backgroundColor:'rgb(97, 95, 95)'}}>Home</button></a>
+
+                </li>
+
+                <li className ="nav-item">
+
+                    <a className ="nav-link" href="#"><button style = {{backgroundColor:'rgb(97, 95, 95)'}}>Explore</button></a>
+
+                </li>
+
+                <li className ="nav-item">
+
+                    <a className ="nav-link" href="#"><button style = {{backgroundColor:'rgb(97, 95, 95)'}}>Search</button></a>
+
+                </li>
+
+            </ul>
+          
+        </nav>
       
     </div>
   );
