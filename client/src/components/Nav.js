3cb1a24c42d2c2f5b08bb7e95d0e51eaa34556e5
Manny <icuxxx2004@yahoo.com>
2019-05-23 11:47:00 -0700

    Added new navbar and modified the validated login form

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -5,13 +5,22 @@ export default function Nav() {
   return (
 
     <div>
-    <nav className="navbar navbar-light bg-light justify-content-between">
-    <a href="/" className="navbar-brand">Navbar</a>
-    <form className="form-inline">
-    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-  </form>
-</nav>
+
+      
+
+      <nav className="navbar navbar-inverse">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <a className="navbar-brand" href="# "><span className="glyphicon glyphicon-headphones">WebSiteName</span></a>
+          </div>
+          <ul className="nav navbar-nav navbar-right">
+            <li><a href="# "><span className="glyphicon glyphicon-user"></span> Sign Up</a></li>
+            <li><a href="# "><span className="glyphicon glyphicon-log-in"></span> Login</a></li>
+          </ul>
+        </div>
+      </nav>
+      
     </div>
   );
 }
+
--- client/src/components/ValidatedLoginForm.js
+++ client/src/components/ValidatedLoginForm.js
@@ -1,9 +1,71 @@
 import React from "react";
+import { Formik } from "formik";
+import * as EmailValidator from "email-validator";
+import * as Yup from "yup";
 
 const ValidatedLoginForm = () => (
-    <div>
-        <h1 style={{}}>Hello World</h1>
-    </div>
+    <Formik
+        initialValues={{ email: "", password: "" }}
+        onSubmit={(values, { setSubmitting }) => {
+            console.log("Submitting");
+        }}
+        
+        //Using Yum for validation
+        validationSchema = {Yup.object().shape({
+            email: Yup.string()
+            .email()
+            .required("Required"),
+            password: Yup.string()
+            .required("No password provided")
+            .min(8, "password is too short - should be 8 characters minimum")
+            .matches(/(?=.*[0-9])/, "Password must contain a number")
+        })}
+    >
+        {props => {
+            const {
+                values,
+                touched,
+                errors,
+                isSubmitting,
+                handleChange,
+                handleBlur,
+                handleSubmit
+            } = props;
+            return (
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="email">Email</label>
+                    <input
+                        name="email"
+                        type="text"
+                        placeholder="Enter your email"
+                        value={values.email}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        className={errors.email && touched.email && "error"}
+                    />
+                    {errors.email && touched.email && (
+                        <div className="Input Feedback">{errors.email}</div>
+                    )}
+                    <label htmlFor="email">Password</label>
+                    <input
+                        name="password"
+                        type="password"
+                        placeholder="Enter your password"
+                        value={values.password}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        className={errors.password && touched.password && "error"}
+                    />
+                    {errors.password && touched.password && (
+                        <div className="Input Feedback">{errors.password}</div>
+                    )}
+                    <button type="submit" disabled={isSubmitting}>Login</button>
+                </form>
+            )
+        }
+        }
+
+    </Formik>
 );
 
 export default ValidatedLoginForm;
